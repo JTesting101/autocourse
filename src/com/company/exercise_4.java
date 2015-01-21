2eb19fa5f4c9f23f0f0cab9c5cbe9294cdf34073
JTesting101 <James_Z@ukr.net>
2015-01-21 20:39:55 +0200

    First commit

---
+++ src/com/company/exercise_4.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by user on 1/19/15.
+ */
+public class exercise_4 {
+    public static void main(String[] args){
+        String str = "one";
+        for (int i = 1; i <= str.length(); i++) {
+            System.out.println(str.toCharArray()[str.length()-i]);
+        }
+    }
+}
