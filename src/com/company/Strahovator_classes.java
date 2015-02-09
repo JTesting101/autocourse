60d97e28275d137ffa0e530fb6d7fee7710ff56c
JTesting101 <James_Z@ukr.net>
2015-02-09 21:08:20 +0200

    First commit

--- src/com/company/Strahovator_classes.java
+++ src/com/company/Strahovator_classes.java
@@ -2,6 +2,7 @@ package com.company;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.*;
@@ -12,67 +13,90 @@ import java.util.*;
 public class Strahovator_classes {
     private static String autoType = "//select[contains(@id,\"ts_type\")]";
     private static String detailsType = "//select[contains(@id,\"ts_details_type\")]";
-    private static String zoneName= "//select[contains(@id,\"ts_zone\")]";
+    private static String zoneName = "//select[contains(@id,\"ts_zone\")]";
     private static String noCrash = "//select[contains(@id,\"no_crash_years_qty\")]";
     private static String period = "//select[contains(@id,\"period\")]";
     private static String dgo = "//select[contains(@id,\"dgo\")]";
+    private static String tooltip = "//div[@class=\"tooltip fade top\"]//div[@class=\"tooltip-inner\"]/p[text()= 'Это та часть страховой выплаты, которую в любом случае необходимо будет оплатить Вам самостоятельно, если произойдет ДТП по Вашей вине']";
+    private static String franchise = "//a[text()=\"Франшиза\"]";
+    private static String submitButton1 = "//table[@class=\"osago_result\"]/tbody/tr/td/button";
+    private static String no_reg = "//strong[text()='без регистрации']";
 
     public static void open() {
         TestHelper.driver.get("https://strahovator.com.ua/kupit_strahovku/avtocivilka_avtograzhdanka_osago_oscpv");
     }
 
-    public static void setAutoType(Integer i){
-        WebElement f = getData.getValueElement(autoType);
+    public static void setAutoType(Integer i) {
+        WebElement f = TestHelper.getData.getValueElement(autoType);
         Select s = new Select(f);
         // We select the value
         s.selectByIndex(i);
     }
 
-    public static void setdetailsType(Integer i){
-        WebElement f = getData.getValueElement(detailsType);
+    public static void setdetailsType(Integer i) {
+        WebElement f = TestHelper.getData.getValueElement(detailsType);
         Select s = new Select(f);
         // We select the value
         s.selectByIndex(i);
     }
 
-    public static void setzoneName(Integer i){
-        WebElement f = getData.getValueElement(zoneName);
+    public static void setzoneName(Integer i) {
+        WebElement f = TestHelper.getData.getValueElement(zoneName);
         Select s = new Select(f);
         // We select the value
         s.selectByIndex(i);
     }
 
-    public static void setnoCrash(Integer i){
-        WebElement f = getData.getValueElement(noCrash);
+    public static void setnoCrash(Integer i) {
+        WebElement f = TestHelper.getData.getValueElement(noCrash);
         Select s = new Select(f);
         // We select the value
         s.selectByIndex(i);
     }
 
-    public static void setperiod(Integer i){
-        WebElement f = getData.getValueElement(period);
+    public static void setperiod(Integer i) {
+        WebElement f = TestHelper.getData.getValueElement(period);
         Select s = new Select(f);
         // We select the value
         s.selectByIndex(i);
     }
 
-    public static void setdgo(Integer i){
-        WebElement f = getData.getValueElement(dgo);
+    public static void setdgo(Integer i) {
+        WebElement f = TestHelper.getData.getValueElement(dgo);
         Select s = new Select(f);
         // We select the value
         s.selectByIndex(i);
     }
 
-    public static String getLogoName(){
-        WebElement n = getData.getValueElement("//tbody//img");
+    public static String getLogoName() {
+        WebElement n = TestHelper.getData.getValueElement("//tbody//img");
         String[] k = n.getAttribute("src").split("/");
+        // We get the last element i.e. <company_short_name>.gif
+        String comp = companies.get(k[k.length-1]);
 
-        return k[-1];
+        return comp;
     }
 
-    private static Map<String,String> companies = new HashMap<String, String>(){
+    public static void getTooltip(){
+        Actions hover = new Actions(TestHelper.driver).moveToElement(TestHelper.getData.getValueElement(franchise));
+        hover.perform();
+        WebElement f = TestHelper.getData.getValueElement(tooltip);
+    }
+
+    public static void pushBuy1(){
+        TestHelper.pushButton(submitButton1);
+    }
+
+    public static void noReg(){
+        TestHelper.pushButton(no_reg);
+    }
+
+    private static Map<String, String> companies = new HashMap<String, String>() {
         {
-         put();
-
+            put("ut.gif","Utico");
+            put("sk.gif","Скайд");
         }
-    }
+    };
+
+
+}
--- src/com/company/Strahovator_tests.java
+++ src/com/company/Strahovator_tests.java
@@ -13,42 +13,56 @@ import java.io.File;
  * Created by user on 2/6/15.
  */
 @RunWith(JUnit4.class)
-public class Strahovator_tests {
+public class Strahovator_tests extends TestHelper{
 
-    @Before
-    public void Init() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:\\My_projects\\aut\\chromedriver_win32\\chromedriver.exe");
-        TestHelper.driver = new ChromeDriver();
-        Strahovator_classes.open();
-
-
-    }
-
-    @After
-    public void CleanUp(){
-
-        //    if(!testPassed){
-        //      File scrFile = ((TakesScreenshot) a).getScreenshotAs(OutputType.FILE);
-        //      FileUtils
-        //     }
-        TestHelper.driver.close();
-    }
 
     // Test 1
     @Test
     public void MyTest()throws InterruptedException {
 
-        Strahovator_classes.setAutoType(0);
-        Strahovator_classes.setdetailsType(0);
-        Strahovator_classes.setdgo(0);
-        Strahovator_classes.setnoCrash(0);
+        Strahovator_classes.setAutoType(1);
+        Strahovator_classes.setdetailsType(1);
+        Strahovator_classes.setdgo(1);
+        Strahovator_classes.setnoCrash(1);
         Strahovator_classes.setperiod(0);
         Strahovator_classes.setzoneName(0);
 
         String company = Strahovator_classes.getLogoName();
+        Assert.assertEquals("Скайд", company);
+        testPassed = true;
 
 
+    }
 
+    // Test 2
+    @Test
+    public void MyTest1() throws InterruptedException{
 
+        Strahovator_classes.setAutoType(1);
+        Strahovator_classes.setdetailsType(1);
+        Strahovator_classes.setdgo(1);
+        Strahovator_classes.setnoCrash(1);
+        Strahovator_classes.setperiod(0);
+        Strahovator_classes.setzoneName(0);
+
+        Strahovator_classes.getTooltip();
+
+        testPassed = true;
+
+    }
+
+    // Test 3
+    @Test
+    public void MyTest2() throws InterruptedException{
+
+        Strahovator_classes.setAutoType(1);
+        Strahovator_classes.setdetailsType(1);
+        Strahovator_classes.setdgo(1);
+        Strahovator_classes.setnoCrash(1);
+        Strahovator_classes.setperiod(0);
+        Strahovator_classes.setzoneName(0);
+
+        Strahovator_classes.pushBuy1();
+        Strahovator_classes.noReg();
     }
 }
